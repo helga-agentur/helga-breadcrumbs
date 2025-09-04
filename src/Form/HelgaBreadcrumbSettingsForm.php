96095e450cfb37f99556a6b20e07f4fdd360c737
Stefanos Petrakis <stefanos.petrakis@gmail.com>
2025-09-04 13:00:31 +0200

    feat(code): add a settings form for admins

---
+++ src/Form/HelgaBreadcrumbSettingsForm.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helga_breadcrumbs\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\system\Entity\Menu;
+
+/**
+ * Configure Helga Breadcrumbs settings for this site.
+ */
+final class HelgaBreadcrumbSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId(): string {
+    return 'helga_breadcrumbs_helga_breadcrumb_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames(): array {
+    return ['helga_breadcrumbs.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $options = [];
+    foreach (Menu::loadMultiple() as $menu) {
+      $options[$menu->id()] = $menu->label();
+    }
+    $form['breadcrumbs_orphans_menu'] = [
+      '#type' => 'select',
+      '#title' => $this->t("Orphan's menu"),
+      '#default_value' => $this->config('helga_breadcrumbs.settings')->get('breadcrumbs_orphans_menu'),
+      '#options' => $options,
+      '#description' => $this->t('Select the menu to use for orphan breadcrumbs.'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $this->config('helga_breadcrumbs.settings')
+      ->set('breadcrumbs_orphans_menu', $form_state->getValue('breadcrumbs_orphans_menu'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
